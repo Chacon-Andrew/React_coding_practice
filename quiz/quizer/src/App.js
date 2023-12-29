2c583ef331aa09670c2a85163d36e32cac7e9143
Andrew Chacon <chaconandrew226@gmail.com>
2023-12-29 18:49:10 -0500

    quizer done

--- quiz/quizer/src/App.js
+++ quiz/quizer/src/App.js
@@ -1,49 +1,73 @@
 import './App.css';
 import React, {useState, useEffect} from 'react'
 
+const answers_beg = [
+  [
+    ["Andrew", true],
+    ["Karl", false],
+    ["Andreew", false],
+    ["Hello!", false]
+  ],
+  [
+    ["Black", false],
+    ["Green", true],
+    ["Pink", false],
+    ["Orange", false]
+  ],
+  [
+    ["17", false],
+    ["21", false],
+    ["69", false],
+    ["23", true]
+  ]
+]
+
+const questions = [
+  ["What is my Name?", answers_beg[0]],
+  ["What is my favorite Color?", answers_beg[1]],
+  ["How old am I?", answers_beg[2]]
+]
+
 function App() {
   const [question, SetQuestion] = useState("")
   const [answers, setAnswers] = useState([])
+  const [index, setIndex] = useState(0)
+  const [selected, setSelected] = useState(false)
 
-  const questions = {
-    "What is my Name?": answers_beg[0],
-    "What is my favorite Color?": answers_beg[1],
-    "How old am I?": answers_beg[2]
+  const handleChange = (e) =>{
+    setSelected(e.target.value === "true")
   }
 
-  const answers_beg = [
-    [
-      {"Andrew": true},
-      {"Karl": false},
-      {"Andreew": false},
-      {"Hello!": false}
-    ],
-    [
-      {"Black": false},
-      {"Green": true},
-      {"Pink": false},
-      {"Orange": false}
-    ],
-    [
-      {"17": false},
-      {"21": false},
-      {"69": false},
-      {"23": true}
-    ]
-  ]
-
-  const buildMap = (questions, answers_beg) => {
-    const map = new Map()
-    for(let i = 0; i < questions.length; i++) map.set(questions[i], answers_beg[i]);
-    return map;
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(selected){
+      setIndex(index + 1)
+    }
   }
 
   useEffect(() => {
+    console.log('render')
+    console.log(index)
+    if(index < questions.length){
+      setAnswers(questions[index][1])
+      SetQuestion(questions[index][0])
+    }
+  }, [question, answers, index])
 
-  }, [question, answers])
   return (
     <div>
-      
+      {
+        index < questions.length ? <form onSubmit={handleSubmit}>
+        <h1>{question}</h1>
+        <div style={{display: 'flex', flexDirection: 'column'}}>{
+          answers.map((answer) => {
+            return (<label key={answer[0]}>{answer[0]} 
+            <input type='radio' value={answer[1]} name='quiz_answers' onChange={handleChange}/></label>)
+          })
+        }</div>
+        <input type='submit' value='submit'/>
+      </form> : <h1>Ur done</h1>
+      }
     </div>
   );
 }
